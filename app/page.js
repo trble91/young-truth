35302705613410dd7fb824c51a2bd0d5db39234c
trble91 <daleyaaron@gmail.com>
2023-07-09 15:10:58 -0400

    adds some files

--- app/page.js
+++ app/page.js
@@ -1,11 +1,18 @@
-"use client"
-import react from "react";
-
+"use client";
+import React from "react";
+import Header from "@/Components/Header.jsx";
+import Homepage from "@/Components/Screens/Homepage.jsx";
+import Footer from "@/Components/Footer.jsx";
+import "./globals.css";
 
 export default function Home() {
   return (
-   <>
-
-   </>
-  )
+    <>
+        <div className="snap-mandatory min-h-screen">
+          <Header />
+          <Homepage />
+          <Footer />
+        </div>
+    </>
+  );
 }
