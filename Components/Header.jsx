ee183a2a67362da89d7bd44d1461556d42af43cf
trble91 <daleyaaron@gmail.com>
2023-07-10 13:50:47 -0400

    adds new dependecies

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -1,7 +1,75 @@
+"use client"
 import React from 'react'
+import { SocialIcon } from "react-social-icons"
+import { motion } from "framer-motion"
+
 
 export default function Header() {
   return (
-    <div>Header</div>
-  )
+    <header className="sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center"> 
+    <motion.div 
+      initial={{
+        x: -500,
+        opacity: 0,
+        scale: 0.5,
+        }}
+      animate={{
+        x: 0,
+        opacity: 1,
+        scale: 1,
+        }}
+      transition={{
+        duration: 0.9,
+        }}
+        className="flex flex-row items-center">
+        <SocialIcon 
+            url="https://www.github.com/trble91"
+            fgColor="grey"
+            bgColor="transparent"
+            />
+
+        <SocialIcon 
+            network="email"
+            url="mailto:daleyaaron@gmail.com"
+            fgColor="grey"
+            bgColor="transparent"
+            />
+
+        <SocialIcon 
+            url="https://www.linkedin.com/in/daleyaaron"
+            fgColor="grey"
+            bgColor="transparent" 
+            />
+    </motion.div>
+    {/* <motion.div 
+      initial={{
+        x: 500,
+        opacity: 0,
+        scale: 0.5,
+        }}
+      animate={{
+        x: 0,
+        opacity: 1,
+        scale: 1,
+        }}
+      transition={{
+        duration: 1.5,
+        }}
+        className="flex flex-row items-center">
+        <SocialIcon 
+            url="https://www.instagram.com"
+            fgColor="gray"
+            bgColor="transparent" />
+        <SocialIcon 
+            url="https://www.facebook.com"
+            fgColor="gray"
+            bgColor="transparent" />
+
+        <SocialIcon 
+            url="https://www.twitter.com"
+            fgColor="gray"
+            bgColor="transparent" />
+    </motion.div> */}
+</header>
+)
 }
