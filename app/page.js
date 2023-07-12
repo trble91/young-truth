808b2f42072482a12ae8346e3943d445a30516e6
trble91 <daleyaaron@gmail.com>
2023-07-12 17:53:41 -0400

    fixes placement issue

--- app/page.js
+++ app/page.js
@@ -8,20 +8,19 @@ import Photography from "@/Components/Photography";
 import Video from "@/Components/Video.jsx";
 import "./globals.css";
 
-
 export default function Home() {
   return (
     <>
       <div className="snap-mandatory min-h-screen">
         <Header />
         <div>
-        <Photography />
-        <Bio />
+          <Photography />
+          <Bio />
+          <div className="relative p-40 pl-20 pr-20 sm:min-w-[400px]">
+            <Video />
+          </div>
         </div>
         {/* <Footer /> */}
-      </div>
-      <div className="relative p-40 pl-20 pr-20 sm:min-w-[400px]">
-         <Video />
       </div>
     </>
   );
