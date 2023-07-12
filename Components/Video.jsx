81de25a18479bf1bf0eed1a68d8ec590ef3ac6cd
trble91 <daleyaaron@gmail.com>
2023-07-12 17:47:39 -0400

    switches video content

--- Components/Video.jsx
+++ Components/Video.jsx
@@ -6,7 +6,7 @@ export default function Video() {
         <iframe
           width="300"
           height="200"
-          src="https://www.youtube.com/embed/JJfyqW5vbeY"
+          src="https://youtu.be/VVumTfYZmJM"
           title="YouTube video player"
           frameborder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
@@ -24,7 +24,7 @@ export default function Video() {
         <iframe
           width="300"
           height="200"
-          src="https://www.youtube.com/embed/NbgP_5aUy4g"
+          src="https://youtu.be/yjjhYEbBCZQ"
           title="YouTube video player"
           frameborder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
