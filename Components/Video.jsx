ca95b1fb44acf669ddfbb197e7f0730ee84e0dc2
trble91 <daleyaaron@gmail.com>
2023-07-15 21:29:08 -0400

    fixes fullScreen

--- Components/Video.jsx
+++ Components/Video.jsx
@@ -8,9 +8,8 @@ export default function Video() {
           height="200"
           src="https://www.youtube.com/embed/VVumTfYZmJM"
           title="YouTube video player"
-          frameborder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowfullscreen
+          allowFullScreen
           className="sm:min-w-[100px]"
         ></iframe>
         <iframe
@@ -20,7 +19,7 @@ export default function Video() {
           title="YouTube video player"
           frameborder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowfullscreen
+          allowFullScreen
           className="sm:min-w-[100px]"
         ></iframe>
         <iframe
@@ -30,7 +29,7 @@ export default function Video() {
           title="YouTube video player"
           frameborder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowfullscreen
+          allowFullScreen
           className="sm:min-w-[100px]"
         ></iframe>
       </div>
