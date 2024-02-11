652417a6d2aa935d5dc3946de2fc248471a0d0ef
trble91 <daleyaaron@gmail.com>
2024-02-11 10:01:23 -0500

    removes unsupported video

--- Components/Video.jsx
+++ Components/Video.jsx
@@ -21,15 +21,7 @@ export default function Video() {
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
           allowFullScreen
         ></iframe>
-        <iframe
-          width="300"
-          height="200"
-          src="https://www.youtube.com/embed/yjjhYEbBCZQ"
-          title="YouTube video player"
-          frameborder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowFullScreen
-        ></iframe>
+      
         </div>
       </div>
   );
