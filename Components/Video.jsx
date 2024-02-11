0904646a4d7dcf2ef1044f800ae7a8247ad3bf69
trble91 <daleyaaron@gmail.com>
2024-02-11 10:05:17 -0500

    recongigs video sizing

--- Components/Video.jsx
+++ Components/Video.jsx
@@ -2,27 +2,35 @@ import React from "react";
 
 export default function Video() {
   return (
-      <div className="flex relative flex-row justify-center snap-center xl:-pt-20 xl:pb-20">
-        <div>
+    <div className="flex relative flex-row justify-center snap-center xl:-pt-20 xl:pb-20">
+      <div>
         <iframe
-          width="300"
-          height="200"
+          width="500"
+          height="300"
           src="https://www.youtube.com/embed/VVumTfYZmJM"
           title="YouTube video player"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
           allowFullScreen
         ></iframe>
         <iframe
-          width="300"
-          height="200"
+          width="500"
+          height="300"
           src="https://www.youtube.com/embed/WsAwmEXpEho"
           title="YouTube video player"
           frameborder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
           allowFullScreen
         ></iframe>
-      
-        </div>
+        <iframe
+          width="500"
+          height="300"
+          src="https://www.youtube.com/embed/GHlG3Egens4?si=ur9cjxmGBXePIcS2"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowfullscreen
+        ></iframe>
       </div>
+    </div>
   );
 }
